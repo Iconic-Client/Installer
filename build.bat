5b0329059d4ad71117628e453f3e123439ffd649
Slddev <sldemonatnoiv@gmail.com>
2022-03-09 09:28:23 +0800

    New GUI!

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-pyinstaller -n IconicClient-Installer -i icon.ico --clean -y --osx-bundle-identifier tk.iconicclient.installer --onefile main.py
+pyinstaller -n IconicClient-Installer -i icon.ico --clean -y --onefile --noconsole main.py
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+from tkinter import font
+import PySimpleGUI as sg
 import os
 import os.path
-import wget
-from os import path
+import urllib.request
 
 path = os.getenv('APPDATA')
 pathversions = path + '\.minecraft\\versions'
@@ -9,25 +10,36 @@ pathiconicfolder = pathversions + '\Iconic-1.8.8'
 urljar = "https://iconicdownloadproxy.netlify.app/Iconic-1.8.8.jar"
 urljson = "https://iconicdownloadproxy.netlify.app/Iconic-1.8.8.json"
 
-print ("Checking if Minecraft versions folder exists: " + str(os.path.isdir(pathversions)))
-if str(os.path.isdir(pathversions)) == "True":
-    print("Downloading Iconic Client 1.8.8")
-    if str(os.path.isdir(pathiconicfolder)) == "False":
-        print(' Make the folder...')
-        os.mkdir(pathiconicfolder)
-        print(' Downloading jar...')
-        wget.download(urljar, pathiconicfolder + '\Iconic-1.8.8.jar')
-        print('\n Downloading json...')
-        wget.download(urljson, pathiconicfolder + '\Iconic-1.8.8.json')
-        print('\n Done!')
-        exit()
-    else:
-        print(' Downloading jar...')
-        wget.download(urljar, pathiconicfolder + '\Iconic-1.8.8.jar')
-        print('\n Downloading json...')
-        wget.download(urljson, pathiconicfolder + '\Iconic-1.8.8.json')
-        print('\n Done!')
-        exit()
-else:
-    print("Make sure you have installed Minecraft.")
-    exit()
+sg.theme("Material2")
+layout = [[sg.Image(r'C:\\Users\sidcd\Downloads\\IconicClient-Installer\\picture.png',size=(75,75))],
+          [sg.Text('Installing Iconic Client', font='Arial')],
+          [sg.Output(size=(50, 10), background_color='black', font='Consolas 8')],
+          [sg.Button('Install')],
+          [sg.Button('Cancel')]]
+window = sg.Window('Iconic Client Installer', layout, margins=(200, 200), element_justification='c', icon=r'C:\\Users\sidcd\Downloads\\IconicClient-Installer\\icon.ico')
+
+while True:  
+    event, values = window.read()
+    if event in (None, 'Install'):
+        if str(os.path.isdir(pathversions)) == "True":
+            print('Started installing Iconic Client 1.8.8')
+            if str(os.path.isdir(pathiconicfolder)) == "False":
+                print('     Make the folder...')
+                os.mkdir(pathiconicfolder)
+                print('     Downloading jar...')
+                urllib.request.urlretrieve(urljar, pathiconicfolder + '\Iconic-1.8.8.jar')
+                print('     Downloading json...')
+                urllib.request.urlretrieve(urljson, pathiconicfolder + '\Iconic-1.8.8.json')
+                print('Done! You can now close this window.')
+            else:
+                print('     Downloading jar...')
+                urllib.request.urlretrieve(urljar, pathiconicfolder + '\Iconic-1.8.8.jar')
+                print('     Downloading json...')
+                urllib.request.urlretrieve(urljson, pathiconicfolder + '\Iconic-1.8.8.json')
+                print('Done! You can now close this window.')
+        else:
+            print('Error Code 1: You might not have installed Minecraft. Contact us on our Discord server.')
+
+    if event in (None, 'Cancel'):
+        break
+window.close()
