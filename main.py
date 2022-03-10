c18b566c5cb12ff2a195713f621f0d08760806da
Slddev <sldemonatnoiv@gmail.com>
2022-03-10 12:43:48 +0800

    Add Changelog Button (Mac)

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ pathversions = '~/Library/Application support/minecraft/versions/'
 pathiconicfolder = pathversions + '/Iconic-1.8.8'
 urljar = "https://iconicdownloadproxy.netlify.app/Iconic-1.8.8.jar"
 urljson = "https://iconicdownloadproxy.netlify.app/Iconic-1.8.8.json"
+popupContent = urllib.request.urlopen("https://download.iconicclient.tk/changelog.txt")
 
 sg.theme("Material2")
 layout = [[sg.Image(r'C:\\Users\sidcd\Downloads\\IconicClient-Installer\\picture.png',size=(75,75))],
           [sg.Text('Installing Iconic Client', font='Arial')],
           [sg.Output(size=(50, 10), background_color='black', font='Consolas 8')],
           [sg.Button('Install')],
-          [sg.Button('Cancel')]]
+          [sg.Button('Changelog')]]
 window = sg.Window('Iconic Client Installer', layout, margins=(200, 200), element_justification='c', icon=r'C:\\Users\sidcd\Downloads\\IconicClient-Installer\\icon.ico')
 
 while True:  
@@ -39,6 +40,8 @@ while True:
         else:
             print('Error Code 1: You might not have installed Minecraft. Contact us on our Discord server.')
 
-    if event in (None, 'Cancel'):
-        break
+    if event in (None, 'Changelog'):
+        for line in popupContent:
+            decoded_line = line.decode("utf-8")
+            print(decoded_line)
 window.close()
